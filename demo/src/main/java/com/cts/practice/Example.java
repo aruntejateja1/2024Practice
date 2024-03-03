0ae72e9fe1ff22a2b659c79d292ffd5cb379156a
aruntejateja1 <aruntejateja1@gmail.com>
2024-03-03 20:26:45 +0530

    u new added

---
+++ demo/src/main/java/com/cts/practice/Example.java
@@ -0,0 +1,12 @@
+package com.cts.practice;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Wrold 2024!");
+
+
+	}
+
+}
