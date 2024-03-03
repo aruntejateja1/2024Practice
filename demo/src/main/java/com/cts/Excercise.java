5d1740aadfd8ddd1bd1f583e5c40187b9e74d453
aruntejateja1 <aruntejateja1@gmail.com>
2024-03-04 00:49:25 +0530

    Excercise 2 class

--- demo/src/main/java/com/cts/Excercise.java
+++ demo/src/main/java/com/cts/Excercise.java
@@ -5,6 +5,7 @@ public class Excercise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Excercise class");
+System.out.println("Excercise2 class");
 	}
 
 }
